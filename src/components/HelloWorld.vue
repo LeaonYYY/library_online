ea7eef7ce04137c8361211a6302cac821c98abcd
Leaon <1530395933@qq.com>
2021-07-22 21:39:06 +0800

    create cli

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-switch v-model="value2" active-color="#13ce66" inactive-color="#ff4949">
+    </el-switch>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      value2: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
